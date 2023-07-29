73a67ef286c02e8497cd85fb0f2b3f01265fb9f9
Andre Cheung <140793321+AndreCheung@users.noreply.github.com>
2023-07-29 12:19:50 +0800

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,32 @@
+#! python3
+# weather.py - Get the current weather info (at Sun Peaks) via OpenWeather API
+# Last update: 20230729
+# Author: Andre Cheung
+# Organizaton: RoboticsCats.com
+import re, requests
+
+# global constants
+OWurl = 'https://api.openweathermap.org/data/3.0/onecall?lat=50.88&lon=-119.89&exclude=minutely,hourly,daily&units=metric&appid={API key}'
+
+def weather(url):
+    # define the regexes
+    temperature = re.compile(r'("temp":)(\d?\d?\d.\d?\d?)')
+    humidity = re.compile(r'("humidity":)(\d?\d?\d)')
+    wind_speed = re.compile(r'("wind_speed":)(\d?\d?\d.\d?\d?)')
+    
+    try:
+        # call the OpenWeather API to get current weather
+        current = requests.get(url)
+
+        if current.status_code == 200:
+            t = temperature.search(current.text)
+            h = humidity.search(current.text)
+            w = wind_speed.search(current.text)
+                      
+            info = t.group(2) +' C | ' + h.group(2) +' % | ' + w.group(2) + ' km/h'
+            return info
+
+    except Exception as e:
+        print(f"Error: {e}")
+        
+print(weather(OWurl))
